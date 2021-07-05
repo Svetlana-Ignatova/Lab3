d409d5828543b64205992dcb9b7a799afab35835
Svetlana-Ignatova <70570788+Svetlana-Ignatova@users.noreply.github.com>
2021-07-05 12:03:19 +0300

    Add files via upload

---
+++ menu.h
@@ -0,0 +1,16 @@
+
+#ifndef S_LAB3_MENU_H
+#define S_LAB3_MENU_H
+
+#include <iostream>
+#include "func.h"
+#include "BinaryTree.h"
+#include "Complex.h"
+
+void sep();
+void Menu();
+void PrintTreeMenu();
+int ToStr();
+void MenuTreeFloat();
+
+#endif //S_LAB3_MENU_H
